28cb4979f2727f8648618404c45da81b4da52e4e
Kristijan <kristijan.stefanoski@yahoo.com>
2019-11-11 13:37:44 +0100

    Reverse GeoCoding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,34 +10,35 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { IonicStorageModule } from '@ionic/storage';
-import {AddressPage} from './modals/address/address.page';
-import {CreditCardPage} from './modals/credit-card/credit-card.page';
-import {AddressPageModule} from './modals/address/address.module';
-import {CreditCardPageModule} from './modals/credit-card/credit-card.module';
-import {Facebook} from '@ionic-native/facebook/ngx';
+import { AddressPage } from './modals/address/address.page';
+import { CreditCardPage } from './modals/credit-card/credit-card.page';
+import { AddressPageModule } from './modals/address/address.module';
+import { CreditCardPageModule } from './modals/credit-card/credit-card.module';
+import { Facebook } from '@ionic-native/facebook/ngx';
 import { AndroidFingerprintAuth } from '@ionic-native/android-fingerprint-auth/ngx';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+
 
 @NgModule({
-    declarations: [AppComponent],
-    entryComponents: [
-        AddressPage,
-        CreditCardPage
-    ],
-    imports: [
-        BrowserModule,
-        IonicModule.forRoot(),
-        AppRoutingModule,
-        IonicStorageModule.forRoot(),
-        AddressPageModule,
-        CreditCardPageModule
-    ],
-    providers: [
-        StatusBar,
-        SplashScreen,
-        { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-        Facebook,
-        AndroidFingerprintAuth
-    ],
-    bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  entryComponents: [
+    AddressPage,
+    CreditCardPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    IonicStorageModule.forRoot(),
+    AddressPageModule,
+    CreditCardPageModule],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    Facebook,
+    AndroidFingerprintAuth,
+    Geolocation],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
